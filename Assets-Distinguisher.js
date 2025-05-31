5e5afc71aaef38c8a92a513db82a4c491a8d374f
HumanMus1c <1714076628@qq.com>
2025-05-31 20:18:49 +0800

    ==UserScript==申明规范化

--- Assets-Distinguisher.js
+++ Assets-Distinguisher.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         Assets-Distinguisher
+// @name:en         Assets-Distinguisher
 // @namespace    https://github.com
 // @version      1.0
-// @description  为GitHub Release Assets添加隔行变色效果 | Add alternating row colors to GitHub Release Assets
+// @description  为GitHub Release Assets添加隔行变色效果。
+// @description:en  Add alternating row colors to GitHub Release Assets.
 // @author       https://github.com/HumanMus1c
 // @match        https://github.com/*/releases*
 // @grant        GM_addStyle
